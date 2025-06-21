efceae40a3538411859ab3877c149f0607338fda
Stefanie Ebert <steffi.123@posteo.net>
2025-06-21 21:26:00 +0200

    feat: animate() jellyfish and pufferfish started

--- models/jelly-fish.class.js
+++ models/jelly-fish.class.js
@@ -5,6 +5,21 @@ class JellyFish extends MovableObject {
         super().loadImage('../img/2.Enemy/2.JellyFish/Regular-damage/Lila1.png');
         this.x = Math.random() * 800; // Random x position within canvas width
         this.y = Math.random() * 600; // Random y position within canvas height
+
+        this.animate(this.y);
     }   
+
+    animate(y){
+        setInterval(() => {
+            this.y += 0.3;
+            if (this.y < 0) {
+                this.y += 0.3; // Move up at a constant speed
+            } else if (this.y > 400) {
+                this.y -= 0.3; // Move down at a constant speed
+            }
+        }, 1000 / 60); // 60 FPS
+        
+        
+    }
 
 }
--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -5,6 +5,20 @@ class PufferFish extends MovableObject {
         super().loadImage('../img/2.Enemy/1.PufferFish_3ColorOptions/1.Swim/1.swim1.png');
         this.x = Math.random() * 800; // Random x position within canvas width
         this.y = Math.random() * 600; // Random y position within canvas height
+
+        this.animate(this.x);
     }
     
+
+    animate(x){
+        setInterval(() => {
+            this.x -= 0.5; // Move left at a constant speed
+            if (this.x < 0) {
+                this.x = 800; // Reset position to the right side of the canvas
+            }
+        }, 1000 / 60); // 60 FPS
+        
+        
+    }
+
 }
