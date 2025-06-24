67c840477b599aa709f5f33cf265eb2073f0c9ed
Stefanie Ebert <steffi.123@posteo.net>
2025-06-24 22:06:02 +0200

    extend animate() for jellyFish and pufferFish

--- models/jelly-fish.class.js
+++ models/jelly-fish.class.js
@@ -1,15 +1,34 @@
 class JellyFish extends MovableObject {
     width = 100;
     height = 100;
+    IMAGES_REGULAR_DAMAGE = [
+        '../img/2.Enemy/2.JellyFish/Regular-damage/Lila1.png',
+        '../img/2.Enemy/2.JellyFish/Regular-damage/Lila 2.png',
+        '../img/2.Enemy/2.JellyFish/Regular-damage/Lila 3.png',
+        '../img/2.Enemy/2.JellyFish/Regular-damage/Lila 4.png',
+        
+    ];
+    currentImage = 0;
+
+
     constructor() {
         super().loadImage('../img/2.Enemy/2.JellyFish/Regular-damage/Lila1.png');
         this.x = Math.random() * 800; // Random x position within canvas width
         this.y = Math.random() * 600; // Random y position within canvas height
-
-        this.animate(this.y);
-    }   
+        this.loadImages(this.IMAGES_REGULAR_DAMAGE);
+       
+        this.animate();
+    }
+ 
 
     animate(y){
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_REGULAR_DAMAGE.length;
+            let path = this.IMAGES_REGULAR_DAMAGE[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        },1000 /5); // 5 frames per second
+
         setInterval(() => {
             this.y += 0.3;
             if (this.y < 0) {
--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -1,16 +1,34 @@
 class PufferFish extends MovableObject {
     width = 100;
     height = 100;
+    IMAGES_SWIM = [
+        '../img/2.Enemy/1.PufferFish_3ColorOptions/1.Swim/1.swim1.png',
+        '../img/2.Enemy/1.PufferFish_3ColorOptions/1.Swim/1.swim2.png',
+        '../img/2.Enemy/1.PufferFish_3ColorOptions/1.Swim/1.swim3.png',
+        '../img/2.Enemy/1.PufferFish_3ColorOptions/1.Swim/1.swim4.png',
+        '../img/2.Enemy/1.PufferFish_3ColorOptions/1.Swim/1.swim5.png',
+    ];
+    currentImage = 0;
+
+
      constructor() {
         super().loadImage('../img/2.Enemy/1.PufferFish_3ColorOptions/1.Swim/1.swim1.png');
         this.x = Math.random() * 800; // Random x position within canvas width
         this.y = Math.random() * 600; // Random y position within canvas height
+        this.loadImages(this.IMAGES_SWIM);
 
         this.animate(this.x);
     }
     
 
     animate(x){
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_SWIM.length;
+            let path = this.IMAGES_SWIM[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 1000 / 5); // 5 frames per second
+
         setInterval(() => {
             this.x -= 0.5; // Move left at a constant speed
             if (this.x < 0) {
