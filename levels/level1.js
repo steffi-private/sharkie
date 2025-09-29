0ea6e25499eb868ebf7ec70ef73f786464e2b17e
Stefanie Ebert <steffi.123@posteo.net>
2025-09-29 12:25:54 +0200

    feature: coins

--- levels/level1.js
+++ levels/level1.js
@@ -2,10 +2,14 @@ const level1 = new Level(
     [
         new PufferFish(),
         new PufferFish(),
-        new PufferFish(),
+        new PufferFish()
+    ],
+    [
         new JellyFish(),
-        new JellyFish(),
-        new FinalEnemy()
+        new JellyFish()
+    ],
+    [
+    new FinalEnemy()
     ],
     [
         new BackgroundObject("../img/3.Background/Layers/5. Water/D1.png", 0, 0),
@@ -29,5 +33,11 @@ const level1 = new Level(
         new BackgroundObject("../img/3.Background/Layers/1. Light/2.png", 720*3, 0),
         new BackgroundObject("../img/3.Background/Layers/2. Floor/D1.png", 720*2, 0),
         new BackgroundObject("../img/3.Background/Layers/2. Floor/D2.png", 720*3, 0),
+    ],
+    [
+        new Coin(300, 250),
+        new Coin(350, 200),
+        new Coin(400, 250),
+        new Coin(350, 300),
     ]
 );
