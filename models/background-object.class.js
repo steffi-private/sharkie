a12b9f50e189dc16b434b620581732d5bfc1d771
Stefanie Ebert <steffi.123@posteo.net>
2025-10-28 22:16:53 +0100

    styling: Barriers added

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -2,11 +2,13 @@ class BackgroundObject extends MovableObject {
     width = 720;
     height = 480;
 
-  constructor(imagePath, x, y) {
+  constructor(imagePath, x, y, width, height) {
       super(); 
       this.loadImage(imagePath);
       this.x = x;
       this.y = y;
+      this.width = width;
+      this.height = height;
   }
 
 }
